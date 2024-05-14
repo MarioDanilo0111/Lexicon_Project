10ee3158a5bdf1f046e3a91c8d92b3760c9ce115
Mario Fernández <nilovinilo1@gmail.com>
2024-05-14 09:00:35 +0200

    Converted Similarities to render in Percentage

--- plotting.py
+++ plotting.py
@@ -2,20 +2,24 @@ import plotly.graph_objects as go
 import numpy as np
 
 def create_similarities_plot(titles, similarities, links):
+
+  # Convert Similaritys to Percentages
+  similarities_percentages = similarities * 100 
+
   fig = go.Figure(data=[go.Scatter(
-    x=similarities,
+    x=similarities_percentages,
     y=np.arange(len(similarities)),
     mode='markers+text',
     text=titles,
     marker=dict(
-      size=20, color=similarities, colorscale='Bluered',
+      size=20, color=similarities_percentages, colorscale='Bluered',
     ),
     customdata=links,
-    hovertemplate="<br>%{text}</b><br><br>Similarity: %{x:.3f}<extra></extra>",
+    hovertemplate="<br>%{text}</b><br><br>Similarity: %{x:.2f}%<extra></extra>",
     )])
   fig.update_layout(
     title='Click on a point to view details: ',
-    xaxis_title="Cousine Similarity",
+    xaxis_title="Cousine Similarity (%)",
     yaxis_title="Documents",
     yaxis=dict(showticklabels=False),
   )
